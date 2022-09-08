c5082c8f505c782b6627649436201aed69ac5353
Jordi Marias I Parella <jordi.marias@i2cat.net>
2022-09-08 17:42:17 +0200

    First deployment of the femiot frontend

---
+++ env.js
@@ -0,0 +1,11 @@
+(function (window) {
+    window.__env = window.__env || {};
+  
+    // API url
+    window.__env.apiUrl = 'ws://84.88.36.186:8883';
+  
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window.__env.enableDebug = true;
+  }(this));
+  
